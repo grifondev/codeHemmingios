32367624c37e5288d77057776f5117da25d6df18
Dmitriy <DM.mitsckevitch@yandex.ru>
2023-04-28 16:18:09 +0300

    Initial Commit

---
+++ codeHemming/codeHemmingApp.swift
@@ -0,0 +1,17 @@
+//
+//  codeHemmingApp.swift
+//  codeHemming
+//
+//  Created by grifondev on 28.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct codeHemmingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
